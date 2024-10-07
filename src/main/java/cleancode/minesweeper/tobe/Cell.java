6dcbd282eef1122057d720c6f6efc043d5c14b88
MoonJeong <dev.moonjeong@gmail.com>
2024-10-07 09:21:27 +0900

    refactor : 사용자의 행위에 따라 Cell의 상태를 변화시키는 방향으로 변수 추가
    sign(String) 기반의 BOARD의 sign을 갈아끼우는 형태에서, 'Cell'이라는 정보를 담은 객체의 상태를 변화시키는 방향으로 변경해준다.

--- src/main/java/cleancode/minesweeper/tobe/Cell.java
+++ src/main/java/cleancode/minesweeper/tobe/Cell.java
@@ -4,37 +4,44 @@ public class Cell {
 
     private static final String FLAG_SIGN = "⚑";
     private static final String LAND_MINE_SIGN = "☼";
-    private static final String CLOSED_CELL_SIGN = "□";
-    private static final String OPENED_CELL_SIGN = "■";
+    private static final String UNCHECKED_SIGN = "□";
+    private static final String EMPTY_SIGN = "■";
 
     private final String sign;
+    private int nearbyLandMineCount;
+    private boolean isLandMine;
 
-    private Cell(String sign) {
+    // Cell이 가진 속성 : 근처 지뢰 숫자, 지뢰 여부
+    // Cell의 상태 : 깃발 유무, 열렸다/닫혔다, 사용자가 확인함
+
+    private Cell(String sign, int nearbyLandMineCount, boolean isLandMine) {
         this.sign = sign;
+        this.nearbyLandMineCount = nearbyLandMineCount;
+        this.isLandMine = isLandMine;
     }
 
-    public static Cell of(String sign) {
-        return new Cell(sign);
+    public static Cell of(String sign, int nearbyLandMineCount, boolean isLandMine) {
+        return new Cell(sign, nearbyLandMineCount, isLandMine);
     }
 
     public static Cell ofFlag() {
-        return of(FLAG_SIGN);
+        return of(FLAG_SIGN, 0, false);
     }
 
     public static Cell ofLandMine() {
-        return of(LAND_MINE_SIGN);
+        return of(LAND_MINE_SIGN, 0, false);
     }
 
     public static Cell ofClosed() {
-        return of(CLOSED_CELL_SIGN);
+        return of(UNCHECKED_SIGN, 0, false);
     }
 
     public static Cell ofOpened() {
-        return of(OPENED_CELL_SIGN);
+        return of(EMPTY_SIGN, 0, false);
     }
 
     public static Cell ofNearbyLandMineCount(int count) {
-       return of(String.valueOf(count));
+       return of(String.valueOf(count), 0, false);
     }
 
     public String getSign() {
@@ -43,7 +50,7 @@ public class Cell {
 
 
     public boolean isClosed() {
-        return CLOSED_CELL_SIGN.equals(this.sign);
+        return UNCHECKED_SIGN.equals(this.sign);
     }
 
     public boolean doesNotClosed() {
