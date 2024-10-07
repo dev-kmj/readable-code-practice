9aa7a8f744ba0b275d3e10e55f9188fb7eb17084
MoonJeong <dev.moonjeong@gmail.com>
2024-10-07 10:35:35 +0900

    refactor : getSign()메서드 수정
    SIGN 상수들을 조건에 따라 return하는 메서드로 수정하고 사용하지 않는 sign 변수 제거

--- src/main/java/cleancode/minesweeper/tobe/Cell.java
+++ src/main/java/cleancode/minesweeper/tobe/Cell.java
@@ -7,26 +7,24 @@ public class Cell {
     private static final String UNCHECKED_SIGN = "□";
     private static final String EMPTY_SIGN = "■";
 
-    private final String sign;
     private int nearbyLandMineCount;
     private boolean isLandMine;
     private boolean isFlagged;
     private boolean isOpened;
 
-    private Cell(String sign, int nearbyLandMineCount, boolean isLandMine, boolean isFlagged, boolean isOpened) {
-        this.sign = sign;
+    private Cell(int nearbyLandMineCount, boolean isLandMine, boolean isFlagged, boolean isOpened) {
         this.nearbyLandMineCount = nearbyLandMineCount;
         this.isLandMine = isLandMine;
         this.isFlagged = isFlagged;
         this.isOpened = isOpened;
     }
 
-    public static Cell of(String sign, int nearbyLandMineCount, boolean isLandMine, boolean isFlagged, boolean isOpened) {
-        return new Cell(sign, nearbyLandMineCount, isLandMine, isFlagged, isOpened);
+    public static Cell of(int nearbyLandMineCount, boolean isLandMine, boolean isFlagged, boolean isOpened) {
+        return new Cell(nearbyLandMineCount, isLandMine, isFlagged, isOpened);
     }
 
     public static Cell create() {
-        return of("", 0, false, false, false);
+        return of(0, false, false, false);
     }
 
     public void turnOnLandMine() {
@@ -62,6 +60,20 @@ public class Cell {
     }
 
     public String getSign() {
-        return sign;
+        if (isOpened) {
+            if (isLandMine) {
+                return  LAND_MINE_SIGN;
+            }
+            if (hasLandMineCount()) {
+                return String.valueOf(nearbyLandMineCount);
+            }
+            return EMPTY_SIGN;
+        }
+
+        if (isFlagged) {
+            return FLAG_SIGN;
+        }
+
+        return UNCHECKED_SIGN;
     }
 }
